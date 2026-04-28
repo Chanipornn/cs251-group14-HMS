488b90e38a466e803a9b9955d9b9d96080060b47
Chayanan <Chayananmariwan@gmail.com>
2026-04-28 15:26:44 +0700

    edit patient entity

--- src/main/java/com/example/demo/model/Patient.java
+++ src/main/java/com/example/demo/model/Patient.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "Patient")
 @Data
@@ -16,34 +18,35 @@ public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PatientID")
     private Integer patientId;
 
-    @Column(nullable = false, length = 64)
+    @Column(name = "Name", nullable = false, length = 64)
     private String name;
 
-    @Column(nullable = false, length = 64)
+    @Column(name = "Surname", nullable = false, length = 64)
     private String surname;
 
-    @Column(length = 1)
+    @Column(name = "Gender", length = 1)
     private Character gender;
 
-    @Column
+    @Column(name = "DateOfBirth")
     private LocalDate dateOfBirth;
 
-    @Column(length = 10)
+    @Column(name = "Telephone", length = 10)
     private String telephone;
 
-    @Column(length = 255)
+    @Column(name = "Address", length = 255)
     private String address;
 
-    @Column(length = 3)
+    @Column(name = "BloodType", length = 3)
     private String bloodType;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "StaffID")
     private Staff staff;
 
-    @Column(unique = true, length = 13)
+    @Column(name = "ThaiNationalID", unique = true, length = 13)
     private String thaiNationalId;
 
     @Column(length = 255)
@@ -61,26 +64,34 @@ public class Patient {
     @Column
     private Float height;
 
+    // User relation
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "UserID", nullable = false, unique = true)
     private UserEntity user;
 
-    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    //@Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Column(name = "created_at", insertable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    //@Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    @Column(name = "updated_at", insertable = false, updatable = false)
     private LocalDateTime updatedAt;
 
+    
     // ความสัมพันธ์กับตารางอื่น
+    @JsonIgnore
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Appointment> appointments;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<MedicalRecord> medicalRecords;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<MedicalCertificate> certificates;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Invoice> invoices;
 }
